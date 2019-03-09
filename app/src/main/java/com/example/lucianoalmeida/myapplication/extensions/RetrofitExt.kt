f3d0e9006e15a9ed008d31882955886fb4c3f5c6
Luciano Almeida <passos.luciano@outlook.com>
2019-03-09 20:41:46 -0300

    Namig property

--- app/src/main/java/com/example/lucianoalmeida/myapplication/extensions/RetrofitExt.kt
+++ app/src/main/java/com/example/lucianoalmeida/myapplication/extensions/RetrofitExt.kt
@@ -6,19 +6,19 @@ import retrofit2.Callback
 import retrofit2.Response
 import kotlin.coroutines.resume
 
-suspend fun <T> Call<T>.await(): T = suspendCancellableCoroutine {
+suspend fun <T> Call<T>.await(): T = suspendCancellableCoroutine { continuation ->
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
             if (response.isSuccessful) {
-                it.resume(response.body()!!)
+                continuation.resume(response.body()!!)
             }
         }
 
         override fun onFailure(call: Call<T>, t: Throwable) {
-            it.resumeWith(Result.failure(t))
+            continuation.resumeWith(Result.failure(t))
         }
     })
-    it.invokeOnCancellation {
+    continuation.invokeOnCancellation {
         this@await.cancel()
     }
 }
