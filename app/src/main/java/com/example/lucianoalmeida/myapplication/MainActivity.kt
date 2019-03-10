87b3435a54f06331c77c7c4e41cb734be752e9fc
Luciano Almeida <passos.luciano@outlook.com>
2019-03-10 11:44:02 -0300

    Implementing list

--- app/src/main/java/com/example/lucianoalmeida/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/lucianoalmeida/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.lucianoalmeida.myapplication
 
+import android.content.Context
+import android.database.DataSetObserver
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.app.ListFragment
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import android.widget.TextView
 import com.example.lucianoalmeida.myapplication.model.Team
 import com.example.lucianoalmeida.myapplication.services.TeamsService
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -23,29 +30,67 @@ class MainActivity: AppCompatActivity(), CoroutineScope {
     override val coroutineContext: CoroutineContext get() =  Dispatchers.Main
 
     val service: TeamsService = TeamsService()
-    var adapterList: ArrayAdapter<Team>? = null
+    lateinit var eastTeamsAdapter: TeamsListAdapter
+    lateinit var westTeamsAdapter: TeamsListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewPager.adapter = TabsPagerAdapter(supportFragmentManager)
+        eastTeamsAdapter = TeamsListAdapter(emptyList(), baseContext)
+        westTeamsAdapter = TeamsListAdapter(emptyList(), baseContext)
+        viewPager.adapter = TabsPagerAdapter(supportFragmentManager, eastTeamsAdapter, westTeamsAdapter)
 
         launch(Dispatchers.IO) {
             val teams = service.teams("ALL")
-            println(teams)
+
             withContext(Dispatchers.Main) {
-                // Update UI
+                updateViews(teams)
             }
         }
     }
 
-    class TabsPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+    private fun updateViews(teams: List<Team>) {
+        eastTeamsAdapter.teams = teams.filter { it.conference == "EAST" }
+        westTeamsAdapter.teams = teams.filter { it.conference == "WEST" }
+        eastTeamsAdapter.notifyDataSetChanged()
+        westTeamsAdapter.notifyDataSetChanged()
+    }
+
+    class TeamsListAdapter(var teams: List<Team>, val context: Context): BaseAdapter() {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val team = teams[position]
+            val textView = TextView(context)
+            textView.text = team.city + " " + team.name
+            return textView
+        }
+
+        override fun getItem(position: Int): Any {
+            return teams[position]
+    }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return teams.size
+        }
+
+    }
+
+    class TabsPagerAdapter(val fm: FragmentManager?,
+                           val eastTeamsAdapter: TeamsListAdapter,
+                           val westTeamsAdapter: TeamsListAdapter) : FragmentPagerAdapter(fm) {
         override fun getCount(): Int {
             return 2
         }
 
         override fun getItem(i: Int): Fragment {
-            return ListFragment()
+            val fragment = ListFragment()
+            fragment.listAdapter = if (i == 0) eastTeamsAdapter else westTeamsAdapter
+            return fragment
         }
 
         override fun getPageTitle(position: Int): CharSequence? {
