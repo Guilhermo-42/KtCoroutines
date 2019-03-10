28c2fcdcc1f25d6e8ef634ac888f229ee3595edd
Luciano Almeida <passos.luciano@outlook.com>
2019-03-10 02:39:27 -0300

    Fixing crash

--- app/src/main/java/com/example/lucianoalmeida/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/lucianoalmeida/myapplication/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.lucianoalmeida.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.*
 
-class MainActivity(override val coroutineContext: CoroutineContext) : AppCompatActivity(), CoroutineScope {
+class MainActivity: AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext get() =  Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 
 }
 
